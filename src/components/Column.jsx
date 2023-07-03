60b0dc7ddfb544f39b56e5bf02884f374c805538
iamkimseunghyun <iamkimseunghyun@gmail.com>
2023-07-03 19:22:59 +0900

    first commit

---
+++ src/components/Column.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useRef, useState } from "react";
+import { useStore } from "../store";
+import "./Column.css";
+import Task from "./Task";
+import classNames from "classnames";
+
+export default function Column({ state }) {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+  const [drop, setDrop] = useState(false);
+
+  const tasks = useStore((store) =>
+    store.tasks.filter((task) => task.state === state)
+  );
+
+  const addTask = useStore((store) => store.addTask);
+  const setDraggedTask = useStore((store) => store.setDraggedTask);
+  const draggedTask = useStore((store) => store.draggedTask);
+  const movedTask = useStore((store) => store.moveTask);
+
+  return (
+    <div
+      className={classNames("column", { drop: drop })}
+      onDragOver={(e) => {
+        setDrop(true);
+        e.preventDefault();
+      }}
+      onDragLeave={(e) => {
+        setDrop(false);
+        e.preventDefault();
+      }}
+      onDrop={(e) => {
+        setDrop(false);
+        movedTask(draggedTask, state);
+        setDraggedTask(null);
+      }}
+    >
+      <div className="titleWrapper">
+        <p>{state}</p>
+        <button onClick={() => setOpen(true)}>Add</button>
+      </div>
+      {tasks.map((task) => (
+        <Task title={task.title} key={task.title} />
+      ))}
+      {open && (
+        <div className="Modal">
+          <div className="modalContent">
+            <input onChange={(e) => setText(e.target.value)} value={text} />
+            <button
+              onClick={() => {
+                addTask(text, state);
+                setText("");
+                setOpen(false);
+              }}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function RefTest() {
+  const ref = useRef();
+
+  useEffect(() => {
+    useStore.subscribe(
+      (store) => store.tasks,
+      (tasks) => {
+        ref.current = tasks;
+      }
+    );
+  }, []);
+
+  return ref.current;
+}
